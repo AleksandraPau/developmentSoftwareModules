e7c3dfe9085e51fe5559d60789c6ddaef2513fe4
AleksandraPaustovskaya <sahsa.com1997@gmail.com>
2026-09-22 10:50:52 +0300

    Add skeleton for the new project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import Greeting from './Greeting';
+// import TaskList from './TaskList';
+// import ActionButton from './ActionButton';
+// import ProfileCard from './ProfileCard';
+// import ImageGallery from './ImageGallery';
+
+function App() {
+  return (
+    <div>
+      <h1>Практическая работа № 1</h1>
+      {/* <Greeting />
+      <hr />
+
+      <TaskList />
+      <hr />
+
+      <ActionButton />
+      <hr />
+
+      <ProfileCard />
+      <hr />
+
+      <ImageGallery />*/}
+    </div> 
+  );
+}
+
+export default App;
